11912af6c82f5c4c6b4371f9320c5397e129bb6c
gaiyadev <gaiyaobed94@gmail.com>
2020-08-09 00:18:31 +0100

    First Commit

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+  Story({String storyTitle, String choice1, String choice2}) {
+    storyTitle = storyTitle;
+    choice1 = choice1;
+    choice2 = choice1;
+  }
+}
